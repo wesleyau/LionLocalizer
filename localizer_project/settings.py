6e81c013a11eeb1a04303b47b0f8e41b8e94242a
Kai Zhao <email@kaizhao.com>
2021-10-10 21:28:26 +0000

    actually wont host static if debug=False

--- localizer_project/settings.py
+++ localizer_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q3=jdwt3d+n*n%1mb^&s%b52c8(%tf%)-tg!*!a#5gs-rs(3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 ALLOWED_HOSTS = ["www.lionlocalizer.org","lionlocalizer.org"]
 
 
