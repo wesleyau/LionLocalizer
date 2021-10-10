16f35dc1815470801b48c87d24430415b4b0b1a4
Kai Zhao <email@kaizhao.com>
2021-10-10 21:19:26 +0000

    fix settings.py again

--- localizer_project/settings.py
+++ localizer_project/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q3=jdwt3d+n*n%1mb^&s%b52c8(%tf%)-tg!*!a#5gs-rs(3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ["lionlocalizer.org"]
+DEBUG = False 
+ALLOWED_HOSTS = ["www.lionlocalizer.org","lionlocalizer.org"]
 
 
 # Application definition
