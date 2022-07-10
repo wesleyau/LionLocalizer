e5a1d04276a031b72c4ac3b0ad77cb0244a0a16b
Wesley <aucwesley@gmail.com>
2022-07-10 08:41:47 -0500

    settings

--- localizer_project/settings.py
+++ localizer_project/settings.py
@@ -232,6 +232,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+import os
+STATIC_ROOT = os.path.join(Base_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
