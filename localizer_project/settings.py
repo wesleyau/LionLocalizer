62909646d97f69fbbe704f653b704296cbad9901
Wesley <aucwesley@gmail.com>
2022-07-19 22:58:16 -0500

    rollback

--- localizer_project/settings.py
+++ localizer_project/settings.py
@@ -264,8 +264,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 import os
-STATIC_ROOT = ""
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
