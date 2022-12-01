5f8523c919816afe3fec2c3e7d5729b153ce9cd0
Wesley <aucwesley@gmail.com>
2022-12-01 13:16:07 -0600

    reference correction

--- frontend/src/components/Homepage/Tabs/References.js
+++ frontend/src/components/Homepage/Tabs/References.js
@@ -43,24 +43,17 @@ const References = () => {
             <Grid item xs={12}>
                 <Typography className={classes.typography}>
                
-                <p>Barnett, R., N. Yamaguchi, B. Shapire, S. Y. W. Ho, I. Barnes, R. Sabin, L. Werdelin, J. Cuisin, & G. Larson, 2014 Revealing the maternal demographic history of <i>Panthera leo</i> using ancient DNA and a spatially explicit genealogical analysis. BMC Evol. Biol. 14.</p>
+                <p>Barnett, R., Sinding, M. H. S., Vieira, F. G., Mendoza, M. L. Z., Bonnet, M., Araldi, A., ... & Gilbert, M. T. P. (2018). No longer locally extinct? Tracing the origins of a lion (Panthera leo) living in Gabon. Conservation Genetics, 19(3), 611-618.</p>
                 
-                Barnett, R., N. Yamaguchi, I. Barnes & A. Cooper, 2006 Lost populations and preserving genetic diversity in the lion <i>Panthera leo</i>: Implications for its ex situ conservation. Conserv. Genet. 7:507–514.
+                Bertola, L. D., Jongbloed, H., Van Der Gaag, K. J., De Knijff, P., Yamaguchi, N., Hooghiemstra, H., ... & De Iongh, H. H. (2016). Phylogeographic patterns in Africa and high resolution delineation of genetic clades in the lion (Panthera leo). Scientific Reports, 6(1), 1-11.
 
-                <p>Barnett, R., N. Yamaguchi, I. Barnes & A. Cooper, 2006 The origin, current diversity and future conservation of the modern lion (<i>Panthera leo</i>). Proc. R. Soc. B Biol. Sci. 273:2119–2125.</p>
+                <p>Bertola, L. D., Tensen, L., Van Hooft, P., White, P. A., Driscoll, C. A., Henschel, P., ... & Vrieling, K. (2015). Autosomal and mtDNA markers affirm the distinctiveness of lions in West and Central Africa. PLoS One, 10(10), e0137975.</p>
                 
-                Bertola, L. D., H. Jongbloed, K. J. van der Gaag, P. de Knijff, N. Yamaguchi, H. Hooghiemstra, H. Bauer, P. Henschel, P. A. White, C. A. Driscoll, T. Tende, U. Ottosson, Y. Saidu, K. Vrieling & H. H. de Iongh, 
-                2016 Phylogeographic patterns in Africa and high resolution delineation of genetic clades in the lion (<i>Panthera leo</i>). Scientific Reports 6:30807.
+                Bertola, L. D., Van Hooft, W. F., Vrieling, K., Uit de Weerd, D. R., York, D. S., Bauer, H., ... & De Iongh, H. H. (2011). Genetic diversity, evolutionary history and implications for conservation of the lion (Panthera leo) in West and Central Africa. Journal of Biogeography, 38(7), 1356-1367.
                 
-                <p>Bertola, L. D., L. Tensen, P. van Hooft, P. A. White, C. A. Driscoll, P. Henschel, A. Caragiulo, I. Dias-Freedman, E. A. Sogbohossou, P. N. Tumenta, T. H. Jrimo, G. R. de Snoo, H. H. de longh, and K. Vrieling, 2015 Autosomal and mtDNA markers affirm the distinctiveness of lions in West and Central Africa. PLOS ONE 10:e0137975.</p>
+                <p>Dubach, J. M., Briggs, M. B., White, P. A., Ament, B. A., & Patterson, B. D. (2013). Genetic perspectives on “lion conservation units” in Eastern and Southern Africa. Conservation Genetics, 14(4), 741-755.</p>
 
-                Bertola, L. D., W. F. van Hooft, K. Vrieling, D. R. Uit de Weerd, D. S. York, H. Bauer, H. H. Y. Prins, P. J. Funston, H. A. Udo de Haes, H. Leirs, W. A. van Haeringen, E. Sogbohossou, P. N. Tumenta, and H. H. de longh, 2011 Genetic diversity, evolutionary history and implications for conservation of the lion (<i>Panthera leo</i>) in West and Central Africa. Journal of Biogeography 38:1356-1367.
-               
-                <p>Burger, J., W. Rosendahl, O. Loreille, H. Hemmer, T. Eriksson, A. Götherström, J. Hiller, M. J. Collins, T. Wess, & K. W. Alt, 2004 Molecular phylogeny of the extinct cave lion <i>Panthera leo spelaea</i>. Mol. Phylogenet. Evol. 30:841-849.</p>
-
-                Dubach, J. M., M. B. Briggs, P. A. White, B. A. Ament, & B. D. Patterson, 2013 Genetic perspectives on "Lion Conservation Units" in Eastern and Southern Africa. Conservation Genetics, 14(4), 741-755.
-                
-                <p>Dubach, J., B. D. Patterson, M. B. Briggs, K. Venzke, J. Flamand, P. Stander, L. Scheepers, & R. W. Kays, 2005 Molecular genetic variation across the southern and eastern geographic ranges of the African lion, <i>Panthera</i>. Conservation Genetics, 6(1), 15-24.</p>
+                Dubach, J., Patterson, B. D., Briggs, M. B., Venzke, K., Flamand, J., Stander, P., ... & Kays, R. W. (2005). Molecular genetic variation across the southern and eastern geographic ranges of the African lion, Panthera leo. Conservation Genetics, 6(1), 15-24.
 
                 </Typography>
 
